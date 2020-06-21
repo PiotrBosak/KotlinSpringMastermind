e2a87c8a6d95a2edc7ce8e7dce059b06c03956dc
piotr <PiotrBosak@yahoo.com>
2020-06-21 18:15:16 +0200

    init

---
+++ src/main/kotlin/pb/spring/kotlinmastermind/KotlinmastermindApplication.kt
@@ -0,0 +1,11 @@
+package pb.spring.kotlinmastermind
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinmastermindApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinmastermindApplication>(*args)
+}
---
+++ src/test/kotlin/pb/spring/kotlinmastermind/KotlinmastermindApplicationTests.kt
@@ -0,0 +1,13 @@
+package pb.spring.kotlinmastermind
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinmastermindApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
